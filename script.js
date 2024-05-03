4ae271a76fba0309bd92a8336de8936ed8891302
Norma Bojorquez <norma_bojorquez26@hotmail.com>
2024-05-03 11:29:48 -0700

    script casi terminado

--- script.js
+++ script.js
@@ -1,192 +1,164 @@
 const axios = require('axios');
 const mongoose = require('mongoose');
+const Planeta = require('./modelos/planetasModelo');
+const Personaje = require('./modelos/personajes');
+const Nave = require('./modelos/navesModelo');
+const Vehiculo = require('./modelos/vehiculos');
+const Pelicula = require('./modelos/peliculasModelo');
 
-mongoose.connect('mongodb://localhost:27017/BACK-SWAPI', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Conexión exitosa a la base de datos');
-    })
-    .catch((error) => {
-        console.error('Error al conectar a la base de datos:', error);
-    });
+mongoose.connect('mongodb://localhost:27017/SWAPI_BACK', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
 
-const planetaSchema = new mongoose.Schema({
-    nombrePlaneta: String, //obligatorio
-    diametro: String,
-    periododeRotacion: String,
-    gravedad: String,
-    poblacion: String,
-    clima: String,
-    terreno: String,
-    superficieAgua: String,
-    fec_creacion: Date,
-    fec_modificacion: Date
+db.on('error', console.error.bind(console, 'Error de conexión a la base de datos:'));
+db.once('open', async () => {
+    console.log('Conexión exitosa a la base de datos');
+
+    try {
+
+        /*const personajesData = await fetchDataRecursive('https://swapi.dev/api/people/');
+        const personajesInstancias = await mapeadoPersonajes(personajesData);
+        await Personaje.insertMany(personajesInstancias);*/
+
+        const planetasData = await fetchDataRecursive('https://swapi.dev/api/planets/');
+        const planetasInstancias = await mapeadoPlanetas(planetasData);
+        await Planeta.insertMany(planetasInstancias);
+        
+        const navesData = await fetchDataRecursive('https://swapi.dev/api/starships/');
+        const navesInstancias = await mapeadoNaves(navesData);
+        await Nave.insertMany(navesInstancias);
+
+        const vehiculosData = await fetchDataRecursive('https://swapi.dev/api/vehicles/');
+        const vehiculosInstancias = await mapeadoVehiculos(vehiculosData);
+        await Vehiculo.insertMany(vehiculosInstancias);
+
+        const peliculasData = await fetchDataRecursive('https://swapi.dev/api/films/');
+        const peliculasInstancias = await mapeadoPeliculas(peliculasData);
+        await Pelicula.insertMany(peliculasInstancias);
+
+        console.log('Datos almacenados en la base de datos');
+        process.exit(0);
+    } catch (error) {
+        console.error('Error al poblar la base de datos:', error);
+        process.exit(1);
+    }
 });
 
-const Planeta = mongoose.model('planetas', planetaSchema);
+const fetchDataRecursive = async (url, data = []) => {
+    try {
+        const response = await axios.get(url);
+        data.push(...response.data.results);
 
-const datosSchema = new mongoose.Schema({
-    nombre: String,
-    clasificacion: String,
-    designacion: String,
-    estatura: String,
-    promedioDeVida: String,
-    colorDeOjos: String,
-    colorDeCabello: String,
-    colorDePiel: String,
-    lenguaje: String,
-    planetaNatal: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'planetas'
-    },
-    fec_creacion: Date,
-    fec_modificacion: Date
-});
-
-const Datos = mongoose.model('especies', datosSchema);
-
-axios.get('https://swapi.dev/api/species')
-    .then(async (response) => {
-        const datosAPI = response.data.results;
-        console.log("Datos de la API:", datosAPI);
-        for (const dato of datosAPI) {
-            try {
-                const planetaResponse = await axios.get(dato.homeworld);
-                const planetaData = planetaResponse.data;
-                console.log("Planeta de la especie:", dato._id)
-                // Crear o encontrar el planeta en la base de datos
-                let planeta = await Planeta.findOne({ nombre: planetaData.name });
-                if (!planeta) {
-                    planeta = new Planeta({
-                        nombre: planetaData.name,
-                        clasificacion: planetaData.classifcation,
-                        designacion: planetaData.designation,
-                        estatura: planetaData.average_height,
-                        promedioDeVida: planetaData.average_lifespan,
-                        colorDeOjos: planetaData.eye_colors,
-                        colorDeCabello: planetaData.hair_colors,
-                        colorDePiel: planetaData.skin_colors,
-                        lenguaje: planetaData.language,
-                        planetaNatal: {
-                            type: mongoose.Schema.Types.ObjectId,
-                            ref: 'Planetas'
-                        },
-                        fec_creacion: planetaData.created,
-                        fec_modificacion: planetaData.created
-
-                    });
-                    await planeta.save();
-                }
-
-                // Crear el nuevo dato de especie con la referencia al planeta
-                const nuevoDato = new Datos({
-                    nombre: dato.name,
-                    clasificacion: dato.classification,
-                    designacion: dato.designation,
-                    estatura: dato.average_height,
-                    promedioDeVida: dato.average_lifespan,
-                    colorDeOjos: dato.eye_colors,
-                    colorDeCabello: dato.hair_colors,
-                    colorDePiel: dato.skin_colors,
-                    lenguaje: dato.language,
-                    planetaNatal: planeta._id, // Utilizando el ObjectId del planeta
-                    fec_creacion: dato.created,
-                    fec_modificacion: dato.edited
-                });
-
-                await nuevoDato.save();
-                console.log('Dato guardado en la base de datos:', nuevoDato);
-            } catch (error) {
-                console.error('Error al guardar el dato:', error);
-            }
+        if (response.data.next) {
+            return await fetchDataRecursive(response.data.next, data);
+        } else {
+            return data;
         }
-    })
-    .catch((error) => {
-        console.error('Error al obtener los datos de la API:', error);
-    });
-//insertar peliculas en la bd
+    } catch (error) {
+        console.log(error)
+    }
+};
+
 /*
-const datosSchema = new mongoose.Schema({
-    tituloPelicula: String,
-    director: String,
-    productor: String,
-    fec_creacion: Date,
-    fec_modificacion: Date
-});
+const mapeadoPersonajes = async (personajesData) => {
+    const personajesInstancias = [];
 
-const Datos = mongoose.model('peliculas', datosSchema);
-
-axios.get('https://swapi.dev/api/films')
-.then((response) => {
-    const datosAPI2 = response.data.results;
-    console.log("Datos get api:", response.data.results)
-    datosAPI2.forEach((dato) => {
-        const nuevoDato = new Datos({
-            tituloPelicula: dato.title,
-            director: dato.director,
-            productos: dato.productor,
-            fec_creacion: dato.created,
-            fec_modificacion: dato.edited
+    for (const personajeData of personajesData) {
+        const personajeInstancia = new Personaje({
+            nombre: personajeData.name,
+            fechaDeNacimiento: personajeData.birth_year,
+            colorDeOjos: personajeData.eye_color,
+            genero: personajeData.gender,
+            colorDeCabello: personajeData.hair_color,
+            altura: personajeData.height,
+            masa: personajeData.mass,
+            colorDePiel: personajeData.skin_color,
+            peliculas: personajeData.films.map((film) => film.split('/')[5]),
+            planetaNatal: personajeData.homeworld.split('/')[5],
+            especies: personajeData.species.map((specie) => specie.split('/')[5]),
+            naves: personajeData.starships.map((starship) => starship.split('/')[5]),
+            vehiculos: personajeData.vehicles.map((vehicle) => vehicle.split('/')[5]),
         });
-        nuevoDato.save()
-            .then(() => {
-                console.log("Dato guardado:", nuevoDato)
-                console.log('Dato guardado en la base de datos:', nuevoDato)
-            })
-            .catch((error) => {
-                console.error('Error al guardar el dato:', error);
-            });
-    });
-})
-.catch((error) => {
-    console.error('Error al obtener los datos de la API:', error);
-});*/
+        personajesInstancias.push(personajeInstancia);
+    }
 
-//insertar peliculas en la bd 
-/*/**/
-/*
-const datosSchema = new mongoose.Schema({
-    nombrePlaneta: String, //obligatorio
-    diametro: String,
-    periododeRotacion: String,
-    gravedad: String,
-    poblacion: String,
-    clima: String,
-    terreno: String,
-    superficieAgua:String,
-    fec_creacion: Date,
-    fec_modificacion: Date
-});
+    return personajesInstancias;
+};*/
 
-const Datos = mongoose.model('planetas', datosSchema);
+// Funciones de mapeo para cada entidad
+const mapeadoPlanetas = async (planetasData) => {
+    const planetasInstancias = [];
 
-axios.get('https://swapi.dev/api/planets/?page=6')
-    .then((response) => {
-        const datosAPI2 = response.data.results;
-        console.log("Datos get api:", response.data.results)
-        datosAPI2.forEach((dato) => {
-            const nuevoDato = new Datos({
-                nombrePlaneta: dato.name,
-                diametro: dato.diameter,
-                periododeRotacion: dato.rotation_period,
-                gravedad: dato.gravity,
-                poblacion: dato.population,
-                clima: dato.climate,
-                terreno: dato.terrain,
-                superficieAgua: dato.surface_water,
-                fec_creacion: dato.created,
-                fec_modificacion: dato.edited
-            }); 
-            nuevoDato.save()
-                .then(() => {
-                    console.log('Dato guardado en la base de datos:', nuevoDato)
-                })
-                .catch((error) => {
-                    console.error('Error al guardar el dato:', error);
-                });
+    for (const planetaData of planetasData) {
+        const planetaInstancia = new Planeta({
+            nombre: planetaData.name,
+            diametro: planetaData.diameter,
+            periododeRotacion: planetaData.rotation_period,	
+            gravedad: planetaData.gravity,
+            poblacion: planetaData.population,
+            clima: planetaData.climate,
+            terrno: planetaData.terrain,
+            superficieAgua: planetaData.surface_water,
         });
-    })
-    .catch((error) => {
-        console.error('Error al obtener los datos de la API:', error);
-    });
+        planetasInstancias.push(planetaInstancia);
+    }
 
-    */
+    return planetasInstancias;
+};
+
+const mapeadoNaves = async (navesData) => {
+    const navesInstancias = [];
+
+    for (const naveData of navesData) {
+        const naveInstancia = new Nave({
+            nombre: naveData.name,
+            modelo: naveData.model,
+            clase: naveData.starship_class,
+            tamano: naveData.length,
+            numPasajeros: naveData.passengers,
+            maxVelocidadAtmosferica: naveData.max_atmosphering_speed,
+            hiperimpulsor: naveData.hyperdrive_rating,
+            MGLT: naveData.MGLT,
+            capacidadCarga: naveData.cargo_capacity,
+            tiempoMaxConsumible: naveData.consumables,
+
+        });
+        navesInstancias.push(naveInstancia);
+    }
+
+    return navesInstancias;
+};
+
+const mapeadoVehiculos = async (vehiculosData) => {
+    const vehiculosInstancias = [];
+
+    for (const vehiculoData of vehiculosData) {
+        const vehiculoInstancia = new Vehiculo({
+            nombre: vehiculoData.name,
+            modelo: vehiculoData.model,
+            clase: vehiculoData.vehicle_class,
+            tamano: vehiculoData.length,
+            numPasajeros: vehiculoData.passengers,
+            maxVelocidadAtmosferica: vehiculoData.max_atmosphering_speed,
+            capacidadMaxima: vehiculoData.cargo_capacity,
+            tiempoMaxConsumible: vehiculoData.consumables
+        });
+        vehiculosInstancias.push(vehiculoInstancia);
+    }
+
+    return vehiculosInstancias;
+};
+
+const mapeadoPeliculas = async (peliculasData) => {
+    const peliculasInstancias = [];
+
+    for (const peliculaData of peliculasData) {
+        const peliculaInstancia = new Pelicula({
+            titulo: peliculaData.title,
+            director: peliculaData.director,
+            productor: peliculaData.producer,
+        });
+        peliculasInstancias.push(peliculaInstancia);
+    }
+
+    return peliculasInstancias;
+};
